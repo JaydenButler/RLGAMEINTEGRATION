0051e8d855bb848411bd3106ab0aac64792c512c
JaydenButler <jaydenbutler4457@gmail.com>
2021-08-24 17:37:18 +1000

    Added base app

---
+++ app.py
@@ -0,0 +1,32 @@
+import json
+import requests
+
+from websocket import create_connection
+
+def subscribe(ws, event, data):
+    ws.send(json.dumps({
+        "event": "wsRelay:register",
+        "data": f"{event}:{data}"
+    }))
+
+
+ws = create_connection("ws://127.0.0.1:49322")
+subscribe(ws, "game", "goal_scored")
+subscribe(ws, "game", "round_started_go")
+subscribe(ws, "game", "statfeed_event")
+
+
+
+while True:
+    result = ws.recv()
+    result = json.loads(result)
+    if(result["event"] == "game:goal_scored"):
+        # Make vmix request on goal scored
+        # requests.get(url = "http://jaydenbutler.com:8088/api/?Function=Cut&Input=2")
+        print("A goal was scored!")
+    elif(result["event"] == "game:round_started_go"):
+        # Make vmix request on goal scored
+        # requests.get("http://jaydenbutler.com:8088/api/?Function=Cut&Input=1")
+        print("Kickoff started!")
+    elif (result["event"] == "game:statfeed_event"):
+        print (result)
